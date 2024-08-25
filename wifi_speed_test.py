adcde9d2a0ab0b93daf24e710eaee127b8fc984b
Guilherme Ferreira Freitas <guilherme.ff@aluno.ufop.edu.br>
2024-08-24 22:20:18 -0300

    "Fixing functions"
    > em connection _speed tinha uma lista sem sentido e reparei agroa;
    
    > atribuição de outras funcionalidades e incrementação de código;
    
    > método download_speed_test() removido;

--- wifi_speed_test.py
+++ wifi_speed_test.py
@@ -3,29 +3,47 @@ import sched
 import speedtest as st
 import time
 
-DELAY_TIME = int(15)
+DELAY_TIME = int(1)
+download = []
+upload = []
+ping = []
 
 stest = st.Speedtest()
-scheduler = sched.scheduler()
+scheduler = sched.scheduler(time.time, time.sleep)
 
 st_download = stest.download()
 st_upload = stest.upload()
 st_ping = stest.results.ping
 stest.get_best_server()
 
+#! FUNÇÃO PARA USAR NOS TESTES
 def df_all_results():
     all_results = stest.results.dict()
     all_results_pd = pd.DataFrame(all_results)
     return all_results_pd
 
-def speed_test():
-    df_results = {'Download': st_download, 'Upload': st_upload, 'Ping': st_ping}
-    df_frame = pd.DataFrame(df_results, columns=['Download', 'Upload', 'Ping'])
-def download_speed_test():
-    df_download = df_all_results()
-    df_download = df_download['download']
-    for x in df_download.index:
-        df_download.loc[x] = 10 ** 6
-    return df_download
+def connection_speed():
+    '''Collect internet speed data'''
+    temp_download = '{:.2f}'.format(stest.download() / 10 ** 6)
+    temp_upload = '{:.2f}'.format(stest.upload() / 10 ** 6)
 
-df_all_results()
+    download.append(temp_download)
+    upload.append(temp_upload)
+    ping.append(st_ping)
+
+    print(f"Next test will execute in {DELAY_TIME}.")
+
+    scheduler.enter(DELAY_TIME, 0, connection_speed)
+
+def df_connection_speed():
+    data = {'Download': download, 'Upload': upload, 'Ping': ping}
+    df_data = pd.DataFrame(data, columns=['Download', 'Upload', 'Ping'])
+
+    scheduler.enter(DELAY_TIME, 1, df_connection_speed)
+
+    return df_data
+
+
+connection_speed()
+#print(df_all_results())
+print(df_connection_speed())
