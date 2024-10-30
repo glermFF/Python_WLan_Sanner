dd4b3d7584469a634b3acd307c705634f9b27082
Guilherme Ferreira Freitas <guilherme.ff@aluno.ufop.edu.br>
2024-10-29 22:58:22 -0300

    Mudando UI
    > Organizando melhor o código e mudando a UI do programa;

--- src/gui/app_window.py
+++ src/gui/app_window.py
@@ -1,9 +1,10 @@
 from customtkinter import *
+from PIL import Image
 from app.wifi_speed_test import *
 from app.wlan_info import *
+from time import sleep
 import threading
 import os
-from time import sleep
 
 file_path = "resources" 
 
@@ -12,54 +13,51 @@ class AppWindow:
     def __init__(self) -> None:
         self.page = CTk()
         self.page.geometry("840x640")
-        self.page.title("Wifi Speed Test")
+        self.page.title("Swosh Scan")
+        self.page.resizable(width=False, height=False)
         self.page.config(bg="#121421")
-
-        self.page.rowconfigure(0, weight=1)
-        self.page.columnconfigure(1, weight=1)
 
         get_wlan_info()
         self.i_info = result["Interface"]
         self.i_ip = result["IP"]
         self.i_mac = result["MAC"]
+
+        #Images
+        self.speed_test_image = CTkImage(dark_image=Image.open('resources/images/control_panel.png'), size=(50, 50))
+        self.scan_image = CTkImage(dark_image=Image.open('resources/images/connection_search.png'), size=(50, 50))
+        self.device_image = CTkImage(dark_image=Image.open('resources/images/router_signal.png'), size=(50, 50))
+
+        #Frames
+        self.side_frame = CTkFrame(master=self.page, fg_color="#f2f2f2", border_width=5, width=125)
+        self.side_frame.pack(side="left", fill="y")
+
+        self.status_info = CTkFrame(self.page, fg_color="#f2f2f2", border_width=5, width=300 , height=300)
+        self.status_info.pack(pady=100, padx=100)
+
+        self.actions = CTkFrame(self.page, fg_color="#000000", border_width=5)
+        self.actions.pack(side="bottom")
+
+        #Buttons
         
-        # Side Frame
-        self.side_frame = CTkFrame(self.page, fg_color="#f2f2f2", corner_radius=3, height=640, width=200)
-        self.side_frame.grid(row=0, column=0, sticky="ns", padx=0, pady=0, ipadx=25)
-        self.side_frame.rowconfigure(1, weight=1)
-        self.side_frame.columnconfigure(0, weight=1)
+        ##Side 
+        self.speed_test = CTkButton(self.side_frame, text="",image=self.speed_test_image, fg_color="transparent", width=75, height=75, corner_radius=5,border_width=3)
+        self.speed_test.pack(padx=30, pady=20)
 
-        self.do_test = CTkButton(self.side_frame, text="Test", fg_color="#583B8A", width=100, corner_radius=5, command=self.test_clicked)
-        self.do_test.grid(row=1, column=0, pady=(0, 150))
+        self.scan = CTkButton(self.side_frame, text="", image=self.scan_image, fg_color="transparent", width=75, height=75, corner_radius=5,border_width=3)
+        self.scan.pack(padx=30, pady=20)
 
-        self.cancel_test = CTkButton(self.side_frame, text="Cancel", state="disabled",fg_color="#583B8A", width=100, corner_radius=5, command=self.cancel_execution)
-        self.cancel_test.grid(row=1, column=0, pady=(0, 0))
+        self.device = CTkButton(self.side_frame, text="", image=self.device_image, fg_color="transparent", width=75, height=75, corner_radius=5,border_width=3)
+        self.device.pack(padx=30, pady=20)
 
-        self.save_data = CTkButton(self.side_frame, text="Save Data", state="disabled",fg_color="#583B8A", width=100, corner_radius=5, command=self.save_data_in_archive)
-        self.save_data.grid(row=1, column=0, pady=(150, 0))
+        ##Actions
+        button_1 = CTkButton(self.actions, text="Test", fg_color="#583B8A", width=100, height=50, corner_radius=5,border_width=3, command=self.test_clicked)
+        button_1.pack(side="left", padx=30, pady=20)
+        
+        self.save_data = CTkButton(self.actions, text="Save Data", fg_color="#583B8A", width=100, height=50, corner_radius=5, state="disabled", border_width=3, command=self.save_data_in_archive)
+        self.save_data.pack(side="left", padx=30, pady=20)
 
-        # Interface Info Label
-        self.device_info = CTkFrame(self.page, corner_radius=3, fg_color="#583B8A", bg_color="transparent")
-        self.device_info.grid(row=0, column=1, pady=(0, 250), padx=(0, 410), sticky="ns")
-
-        self.text = CTkLabel(self.device_info, text="Wlan Interface", font=("Arial", 24))
-        self.text.grid(padx=50 , pady=10)
-
-        self.info = CTkLabel(self.device_info, text=f"Interface: {self.i_info}", font=("Arial", 12))
-        self.info.grid(padx=50, pady=10)
-
-        self.info = CTkLabel(self.device_info, text=f"Ip (actual network): {self.i_ip}", font=("Arial", 12))
-        self.info.grid(padx=50, pady=10)
-
-        self.info = CTkLabel(self.device_info, text=f"Mac: {self.i_mac}", font=("Arial", 12))
-        self.info.grid(padx=50, pady=10)
-
-        # Terminal Label
-        self.terminal_frame = CTkFrame(self.page, fg_color="#000000", corner_radius=3)
-        self.terminal_frame.grid(row=0, column=1, padx=30, pady=(465, 10), sticky="ns", ipadx=600)
-
-        self.loading_label = CTkLabel(self.terminal_frame, text="", corner_radius=3, bg_color="transparent")
-        self.loading_label.grid(padx=5, pady=5)
+        button_2 = CTkButton(self.actions, text="Cancel", fg_color="#583B8A", width=100, height=50, corner_radius=5, state="disabled", border_width=3, command=self.cancel_execution)
+        button_2.pack(side="left", padx=30, pady=20)        
 
         self.page.mainloop()
 
